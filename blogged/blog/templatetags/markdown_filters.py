eb5f158d0d3aafb84d4746fc6b92cde5ab5f09ff
jannsta1 <jannsta1@gmail.com>
2026-02-12 10:44:39 +0000

    chore: format lists in html

--- blogged/blog/templatetags/markdown_filters.py
+++ blogged/blog/templatetags/markdown_filters.py
@@ -10,5 +10,9 @@ def render_markdown(text: str) -> str:
     """
     Converts markdown text to HTML.
     """
-    return mark_safe(markdown.markdown(text, extensions=["fenced_code"]))
+    return mark_safe(
+        markdown.markdown(
+            text, extensions=["extra", "nl2br", "fenced_code", "md_in_html"]
+        )
+    )  # "attr_list", "smarty", "nl2br",
     # for more extensions see https://python-markdown.github.io/extensions/
